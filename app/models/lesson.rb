dc6ad23e5b2b673cfd02e142d13d93eb8fc37ec2
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-05-17 10:25:11 +0000

    Added video uploader

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,9 @@
 class Lesson < ActiveRecord::Base
 
 	belongs_to :section
+	mount_uploader :video, VideoUploader
+
+	def video
+    	"https://firehose-lessons-ken.s3.amazonaws.com/uploads/lesson/video/1/SampleVideo_1280x720_1mb.mp4"
+  	end
 end
