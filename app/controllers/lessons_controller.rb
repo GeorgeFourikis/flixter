861bf1137b25f49b3299ba7d728ca6ef37fcda9e
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-05-18 14:54:58 +0000

    Authentication on lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,8 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_lesson
 	def show
+
 	end
 
 	private
@@ -8,4 +11,13 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_authorized_for_current_lesson
+  	if current_lesson.section.course.user != current_user.enrolled_in?(@course)
+  		redirect_to courses_path(@course), alert:'You are not enrolled,you cannot see the lesson!'
+    else
+    	redirect_to lessons_path(@lesson)
+  	end
+  end
+
 end
