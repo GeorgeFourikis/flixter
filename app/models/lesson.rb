03e38bee0e3f0f512491e0dbd0eb792af148adef
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-05-20 18:03:19 +0000

    Added JS and AJAX for drag-drop functionality

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ActiveRecord::Base
 
 	belongs_to :section
-	mount_uploader :video, VideoUploader
+	#mount_uploader :video, VideoUploader
 
 	def video
     	"https://firehose-lessons-ken.s3.amazonaws.com/uploads/lesson/video/1/SampleVideo_1280x720_1mb.mp4"
