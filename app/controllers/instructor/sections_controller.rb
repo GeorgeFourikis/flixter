3da2aed1866e3d6a34543bc63545a28080fb827a
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-05-24 10:40:55 +0000

    first deploy on Ranked Sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -11,6 +11,11 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render text: 'updated!'
+  end
+
   private
 
   def require_authorized_for_current_course
@@ -25,6 +30,6 @@ class Instructor::SectionsController < ApplicationController
   end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
