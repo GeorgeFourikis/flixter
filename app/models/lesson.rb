a8892651485585be5e6aa3bc2bf58de3b632ed25
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-05-25 15:35:54 +0000

    Added Next Lesson button

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,10 +3,19 @@ class Lesson < ActiveRecord::Base
 	belongs_to :section
 	#mount_uploader :video, VideoUploader
 
-	def video
+  def video
     	"https://firehose-lessons-ken.s3.amazonaws.com/uploads/lesson/video/1/SampleVideo_1280x720_1mb.mp4"
-  	end
+  end
 
-  include RankedModel
-  ranks :row_order, :with_same => :section_id
+	  include RankedModel
+	  ranks :row_order, :with_same => :section_id
+
+  def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+
+    if lesson.blank? && section.next_section
+      return section.next_section.lessons.rank(:row_order).first
+    end
+    return lesson
+  end
 end
